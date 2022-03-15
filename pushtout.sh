9caeb2868dab82105af41d7d17ebccf8bce4b4b2
remycrochon <crochonremy@gmail.com>
2022-03-15 14:39:50 +0100

    config files on 15-03-2022 14:39:50

---
+++ pushtout.sh
@@ -0,0 +1,26 @@
+# Allez dans le dossier /config ou
+# Remplacez-le par votre dossier de configuration Home Assistant s'il est différent
+##cd /config
+
+# Ajouter tous les fichiers au référentiel en respectant les règles .gitignore
+##git add .
+
+# Valider les modifications avec le message de l'horodatage actuel
+##git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+
+# Pousser les changements vers GitHub
+##git push -u origin master
+
+
+#!/bin/bash
+date >> /config/git-last.txt
+cd /home/remy/SynologyDrive/25-Eagle/projects
+git config --global user.email "crochonremy@gmail.com"
+git config --global user.name "remycrochon"
+git add .
+git status
+# Commit changes with message with current date stamp
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+# Push changes towards GitHub
+git push -u origin main
+exit
